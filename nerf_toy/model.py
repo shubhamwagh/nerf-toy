5a6663c216cc3ce6a42ea72119a0dd0a4541c00b
Shubham Wagh <shubhamwagh48@gmail.com>
2022-10-14 16:17:38 +0100

    Refactor model with additional arguments

--- nerf_toy/model.py
+++ nerf_toy/model.py
@@ -1,17 +1,22 @@
 import tensorflow as tf
 from tensorflow.keras import backend as K
 
+from typing import Tuple
 
-def base_model(input_shape, output_dim=3):
+
+def base_model(input_shape: Tuple[int, int, int], output_dim: int = 3, num_layers: int = 4, num_channels: int = 256):
+    """
+    Simple 4-layer MLP (3-hidden layers + 1 output layer) using conv layers of kernel size = 1
+    """
     channel_axis = 1 if K.image_data_format() == "channels_first" else -1
     if K.image_data_format() == "channels_first":
         x = inputs = tf.keras.layers.Input(shape=(input_shape[-1], input_shape[0], input_shape[1]))
     else:
         x = inputs = tf.keras.layers.Input(shape=input_shape)
 
-    for _ in range(3):
-        x = tf.keras.layers.Conv2D(256, kernel_size=1, padding='valid', activation='relu')(x)
-        x = tf.keras.layers.BatchNormalization(axis=-1)(x)
+    for _ in range(num_layers - 1):
+        x = tf.keras.layers.Conv2D(num_channels, kernel_size=1, padding='valid', activation='relu')(x)
+        x = tf.keras.layers.BatchNormalization(axis=channel_axis)(x)
 
     x = tf.keras.layers.Conv2D(output_dim, kernel_size=1, padding='valid', activation='sigmoid')(x)
     return tf.keras.Model(inputs, x, name='base_nerf_model')
