b4f6268914d0bc4eb608b27113fc012ce7d129a9
shubham <shubham@Dell-G5-5587>
2022-10-15 00:01:28 +0100

    Added plotting callback

--- nerf_toy/callback.py
+++ nerf_toy/callback.py
@@ -2,23 +2,24 @@ import os
 import imageio
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from nerf_toy.utils import arr_to_image
 
 from typing import List
 
 
-class NerfToyPredictionSaverCallback(tf.keras.callbacks.Callback):
+class PredictionVideoSaverCallback(tf.keras.callbacks.Callback):
     def __init__(self, x_test, y_test, every: int = 1, video_path='/tmp'):
-        super(NerfToyPredictionSaverCallback, self).__init__()
+        super(PredictionVideoSaverCallback, self).__init__()
         self.x_test = x_test
         self.y_test = y_test
         self.every = every
         self.prediction_imgs: List[np.uint8] = []  # uint8
         self.video_path = video_path
+        self.saved_path = ''
 
     def on_epoch_end(self, epoch, logs=None):
         if epoch % self.every == 0:
-            print('Evaluating Model...')
             pred = self.model(self.x_test, training=False)
             self.prediction_imgs.append(arr_to_image(pred.numpy()[0]))
 
@@ -32,4 +33,40 @@ class NerfToyPredictionSaverCallback(tf.keras.callbacks.Callback):
 
         file_path = os.path.join(self.video_path, f'training_convergence_{count}.mp4')
         imageio.mimwrite(file_path, data8, fps=20)
+        self.saved_path = file_path
         print(f'Saved training convergence video as {file_path}')
+
+
+class PlotLossesAndMetricsCallback(tf.keras.callbacks.Callback):
+    def __init__(self):
+        super(PlotLossesAndMetricsCallback, self).__init__()
+        self.losses = []
+        self.psnrs = []
+        self.ssims = []
+        self.epochs = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.epochs.append(epoch)
+        self.losses.append(logs.get('loss'))
+        self.psnrs.append(logs.get('psnr'))
+        self.ssims.append(logs.get('ssim'))
+
+    def on_train_end(self, logs=None):
+        print('Plotting training metrics and losses...')
+        fig, ax = plt.subplots(1, 3, figsize=(30, 50))
+        ax[0].set_title('Train loss')
+        ax[0].plot(self.epochs, self.losses, label='train loss')
+        ax[0].set_ylabel('loss')
+        ax[0].set_xlabel('epochs')
+
+        ax[1].set_title('PSNR')
+        ax[1].plot(self.epochs, self.psnrs, label='psnr')
+        ax[1].set_ylabel('PSNR')
+        ax[1].set_xlabel('epochs')
+
+        ax[2].set_title('SSIM')
+        ax[2].plot(self.epochs, self.ssims, label='ssim')
+        ax[2].set_ylabel('ssim')
+        ax[2].set_xlabel('epochs')
+
+        plt.show()
