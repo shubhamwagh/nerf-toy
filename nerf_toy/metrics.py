b62db64d1dcdbf49e312798634007e46daeadae0
Shubham Wagh <shubhamwagh48@gmail.com>
2022-10-14 17:49:30 +0100

    Updated metrics

--- nerf_toy/metrics.py
+++ nerf_toy/metrics.py
@@ -1,16 +1,22 @@
 import tensorflow as tf
 
 
-def psnr(y_true, y_pred):
-    """
-    Peak Signal to Noise ratio
-    """
-    return tf.image.psnr(y_true, y_pred, 1.0, name='PSNR')
+def PSNR(max_value: float = 1.0):
+    def psnr(y_true, y_pred):
+        """
+        Peak Signal to Noise ratio
+        """
+        return tf.image.psnr(y_true, y_pred, max_val=max_value, name='PSNR')
+
+    return psnr
 
 
-def ssim(y_true, y_pred):
-    """
-    Structured similarity index
-    """
-    return tf.image.ssim(y_true, y_pred, max_val=1.0, filter_size=11,
-                         filter_sigma=1.5, k1=0.01, k2=0.03)
+def SSIM(max_value: float = 1.0):
+    def ssim(y_true, y_pred):
+        """
+        Structured similarity index
+        """
+        return tf.image.ssim(y_true, y_pred, max_val=max_value, filter_size=11,
+                             filter_sigma=1.5, k1=0.01, k2=0.03)
+
+    return ssim
