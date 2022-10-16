2b906be16c4d41264f936d3bbdaab91ea5f3d997
shubham <shubham@Dell-G5-5587>
2022-10-16 16:35:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md") as f:
 
 setup(
     name='nerf_toy',
-    version='1.0.0',
+    version='0.0.1',
     description='A 2D toy illustration of Neural Radiance Fields',
     url='https://github.com/shubhamwagh/nerf-toy',
     long_description=README,
